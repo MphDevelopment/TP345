7fc9c81c92cdd0c92c5b31844a049ac55b5b6bc5
theo gouzien <jespar@protonmail.com>
2018-01-31 12:11:57 +0100

    deplacer SearchHtml vers application

--- src/test/SearchHtmlTest.scala
+++ src/test/SearchHtmlTest.scala
@@ -1,5 +1,6 @@
 package test
 import library._
+import application.SearchHtml
 
 object SearchHtmlTest extends App {
   println(SearchHtml.filtreHtml(ExempleHtml.exemple2, ExpressionParser.readExp))
