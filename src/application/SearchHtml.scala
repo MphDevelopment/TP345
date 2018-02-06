2374478aa8d5ba876e7ccb438a6d46152e4bfc56
theo gouzien <jespar@protonmail.com>
2018-02-06 08:55:02 +0100

    Commentaires des fonctions de SearchHtml

--- src/application/SearchHtml.scala
+++ src/application/SearchHtml.scala
@@ -10,6 +10,13 @@ import library.Text
 import library.Word
 
 object SearchHtml extends FiltrageHtml {
+  
+  /** vérifie qu'une page contient une expression
+   *  
+   * @param h La page html à tester
+   * @param e L'expression à rechercher dans cette page
+   * @return true si l'expression est trouvable dans la page
+   */
   def filtreHtml(h:Html,e:Expression):Boolean= {
     e match {
       case And(e1, e2) => (filtreHtml(h, e1) && filtreHtml(h, e2))
@@ -18,6 +25,12 @@ object SearchHtml extends FiltrageHtml {
     }
   }
 
+  /** vérifie qu'une page contient bien un mot
+   * 
+   * @param h La page html à tester
+   * @param w Le mot à trouver
+   * @return true si la page contient bien le mot
+   */
   private def findHtml(h:Html, w:String):Boolean= {
     h match {
       case Tag(_, _, children) => {
@@ -27,6 +40,7 @@ object SearchHtml extends FiltrageHtml {
         }
         res
       }
+      // On cherche le mot sans vérifier qu'il est isolé (pas utile pour l'instant)
       case Text(t) => t contains w
     }
   }
