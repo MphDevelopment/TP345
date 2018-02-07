ed891f000058b64f688918af5a0abbb802e14f21
MphDevelopment <theo.jammes@gmail.com>
2018-02-07 11:00:50 +0100

    Application.scala

--- src/application/Appli.scala
+++ src/application/Appli.scala
@@ -2,17 +2,18 @@ package application
 import java.io.FileWriter
 import java.util.Scanner
 import library.ExpressionParser
+import Analyseur
 
 object Appli extends App {
+  println("Recherche :")
   val recherche = ExpressionParser.readExp
   
-  
-  //recherche2url
+  //recherche2url dans l'analyseur.
 
-  val resultats: List[(String, String)] = List() //AnalysePage.resultats
+  val resultats: List[(String, String)] = Analyseur.resultat(recherche)
   val text: String = HtmlWriter.process(ProductionHtml.resultat2html(resultats))
   
-  val file = new FileWriter("resultat.txt")
+  val file = new FileWriter("resultat.html")
   try {
     file.write(text)
   } finally file.close()
