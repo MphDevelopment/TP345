cdb9d3cd01108afb3a1bd87056725c601a90dbcb
jespar-dev <jespar@protonmail.com>
2018-02-08 20:17:05 +0100

    On peut maintenant chercher des phrases entières

--- src/library/Expression.scala
+++ src/library/Expression.scala
@@ -12,59 +12,89 @@ case class Or(e1:Expression,e2:Expression) extends Expression
 
 /** Le parseur d'expressions */
 object ExpressionParser{
-  
-  /** La méthode principale du parseur: lit (au clavier) une chaîne de caractères de la forme "((toto and titi) and (tata or tutu))" et produit une 
+
+  /** Remet les espaces à leurs place dans l'expression
+    * @param exp l'expression
+    * @return l'expression avec des espaces
+    */
+  def putSpaces(exp:Expression):Expression= {
+    exp match {
+      case Word(w) => Word(w.replace('_', ' '))
+      case And(e1, e2) => And(putSpaces(e1), putSpaces(e2))
+      case Or(e1, e2) => Or(putSpaces(e1), putSpaces(e2))
+    }
+  }
+
+  /** La méthode principale du parseur: lit (au clavier) une chaîne de caractères de la forme "((toto AND titi) AND (tata OR tutu))" et produit une
       expression. La méthode itère le processus jusqu'à ce qu'une chaîne de caractère pouvant être transformée en une expression
       est tapée.
-   * @return l'expression résultat du parsing.
-   */ 
+    * @return l'expression résultat du parsing.
+    */
   def readExp={
     var rep=""
     var query:Expression= Word("")
     while (rep==""){
-        println("Donnez votre requète sous forme de mots clés et combinés avec and/or\nPar exemple: developpeur and (rennes or nantes) and (python or java)")
-        rep = readLine()
-        val p= LocalParser.parse(rep)
-        if (p.successful) query=p.get
-        else {println("Malformed query!"); rep =""}
+      println("Donnez votre requète sous forme de mots clés et combinés avec and/or\nPar exemple: developpeur AND (rennes OR nantes) AND (python OR java)")
+      rep = readLine()
+
+      /*
+      Pour pouvoir chercher des phrases entiére, on doit renplacer les espaces par d'autres caractères
+      (il existe surement un moyen plus "propre" de le faire, mais nous avons réussi à trouver malgré
+      la doc de StdLexical et StdTokensParser)
+      */
+      rep = rep.replace(' ', '_').replaceAll("_OR_", " OR ")
+        .replaceAll("_AND_", " AND ")
+
+
+      val p= LocalParser.parse(rep)
+      if (p.successful) query=p.get
+      else {println("Malformed query!"); rep =""}
     }
+
+    /*
+    On remet les espaces
+     */
+    query = putSpaces(query)
+
+    // TODO debug
+    println(query)
     query
   }
-  
+
   /** L'objet local qui implémente le parseur */
   object LocalParser extends StdTokenParsers{
-     type Tokens = StdLexical
+    type Tokens = StdLexical
 
-  import lexical.{ Keyword, NumericLit, StringLit, Identifier }
+    import lexical.{ Keyword, NumericLit, StringLit, Identifier }
 
-  val lexical = new StdLexical
+    val lexical = new StdLexical
 
-  lexical.reserved += ("and", "or")
-  lexical.delimiters ++= List("(", ")")
+    lexical.reserved += ("AND", "OR")
+    lexical.delimiters ++= List("(", ")")
 
-  /** Lit au clavier une chaîne de caractères de la forme "((toto and titi) and (tata or tutu))" et produit une liste
-   *  des identifiants recontrés dans la chaîne.
+    /** Lit au clavier une chaîne de caractères de la forme "((toto AND titi) AND (tata OR tutu))" et produit une liste
+      *  des identifiants recontrés dans la chaîne.
    	  @param s la chaîne de caractères à analyser
-   	  @return un objet de type parseResult qui contient l'expression résultat du parsing, si le parsing a réussi.
-   */
-  
-  def parse(s:String):ParseResult[Expression]={
-    expr(new lexical.Scanner(s))
+    @return un objet de type parseResult qui contient l'expression résultat du parsing, si le parsing a réussi.
+      */
+
+    def parse(s:String):ParseResult[Expression]={
+      expr(new lexical.Scanner(s))
+    }
+
+    // the parser itself
+    def expr: Parser[Expression] = andExp | orExp | unExp
+    def factor: Parser[Expression] = "(" ~> expr <~ ")"
+
+    def andExp: Parser[Expression] = (unExp ~ "AND" ~ expr ^^ { case x ~ _ ~ y => And(x,y) })
+
+    def orExp: Parser[Expression] = (unExp ~ "OR" ~ expr ^^ { case x ~ _ ~ y => Or(x,y) })
+
+    def unExp: Parser[Expression] = word | factor
+
+    def word: Parser[Expression] = (
+      ident ^^ { case s => Word(s) }
+        | numericLit ^^ {case i => Word(i.toString)})
   }
-  
-  // the parser itself
-  def expr: Parser[Expression] = andExp | orExp | unExp
-  def factor: Parser[Expression] = "(" ~> expr <~ ")"
-
-  def andExp: Parser[Expression] = (unExp ~ "and" ~ expr ^^ { case x ~ _ ~ y => And(x,y) })
-
-  def orExp: Parser[Expression] = (unExp ~ "or" ~ expr ^^ { case x ~ _ ~ y => Or(x,y) })
-  
-  def unExp: Parser[Expression] = word | factor
-  
-  def word: Parser[Expression] = (
-      ident ^^ { case s => Word(s) } 
-  	| numericLit ^^ {case i => Word(i.toString)})
-   }
 
 }
